da82e2f72bc8760a83c54959b5717e06ad13ab6b
Danny Smith <hi@danny.is>
2017-03-16 14:03:06 +0000

    Deploy issues

--- app.rb
+++ app.rb
@@ -1,22 +1,25 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'httparty'
 require 'pry'
 
-def call_api(path)
-  HTTParty.get "http://lacedeamon.spartaglobal.com/todos#{path}"
-end
+class DemoApp < Sinatra::Base
 
-get '/' do
-  @todos = call_api('/')
-  binding.pry
-  erb :index
-end
+  def call_api(path)
+    HTTParty.get "http://lacedeamon.spartaglobal.com/todos#{path}"
+  end
 
-get '/new' do
-  erb :create_todo
-end
+  get '/' do
+    @todos = call_api('/')
+    binding.pry
+    erb :index
+  end
 
-get '/:id' do |id|
-  @todo = call_api("/#{id}")
-  erb :todo
+  get '/new' do
+    erb :create_todo
+  end
+
+  get '/:id' do |id|
+    @todo = call_api("/#{id}")
+    erb :todo
+  end
 end
---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'rack'
+
+# Load the Sinatra App
+require File.dirname(__FILE__) + '/app'
+
+run DemoApp
